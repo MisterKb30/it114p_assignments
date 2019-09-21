35a01c4ca220e56976ea9939d5ed3fa6890eaaf0
Kenneth Bonilla <bonillakenneth30@gmail.com>
2019-09-21 22:16:25 +0800

    Assignment 4 - Make use of CSS Framework to our web project

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
-
+const port = 3001;
+//Set default template engine
 app.set('view engine', 'ejs');
 
-const accountRoutes = require('./routes/userAccount')
-const viewRoutes = require('./routes/viewUser')
+//Include custom routes
+const createData = require('./routes/create');
+const displayData = require('./routes/display');
 
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use(accountRoutes);
-app.use(viewRoutes);
-
-app.listen(3000);
+//To parse incoming body, without it you will get undefined value.
+app.use(bodyParser.urlencoded({ extended: false }));
+//To set our public files such as css and js
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css',express.static(path.join(__dirname,'node_modules/bulma/css')));
+app.use('/font',express.static(path.join(__dirname,'node_modules/@mdi/font')));
+//We tell our app to custom routes that we had included
+app.use(createData.routes);
+app.use(displayData);
+//Default route when page is not found
+app.use((req, res, next) => {
+  res.status(404).render('404',
+    {
+      pageTitle: '404 - Page Not Found',
+      path: ""
+    });
+});
+app.listen(port, () => console.log (`Server running at http://localhost:${port}`));
